39b52d345c3f6e027320229b522d6a8be75a6f28
Marcel Bretschneider <info@3epnm.de>
2019-01-03 17:26:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# express-sharp-server
+
+express-sharp-server is a middleware that implements a restful image server based on node-sharp.
---
+++ index.js
@@ -0,0 +1,9 @@
+/*!
+ * express-simple-imagesrv
+ * Copyright(c) 2019 Marcel Bretschneider
+ * Apache 2.0 Licensed
+ */
+
+'use strict';
+
+module.exports = require('./lib/ImgSrv');
